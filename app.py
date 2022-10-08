56ab42312cc39af8730b3dc08f4de4784716eecf
Karan Mandve <59260030+karanmandve@users.noreply.github.com>
2022-10-08 11:41:37 +0530

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 # HOMEPAGE
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
