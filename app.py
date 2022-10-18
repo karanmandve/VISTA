f41599b436272382bb5061f07e4659d7972e9f5f
Karan <karanmandve1234@gmail.com>
2022-10-18 10:11:37 +0530

    remove teachers login and add dashboard

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ cors = CORS(app)
 # HOMEPAGE
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    return render_template("index.html")\
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
