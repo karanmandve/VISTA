40398c509852601e0fcde355cfcab344c7af995b
Karan <karanmandve1234@gmail.com>
2022-10-18 10:33:21 +0530

    testing

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ cors = CORS(app)
 # HOMEPAGE
 @app.route("/")
 def homepage():
-    return render_template("index.html")\
+    return render_template("index.html")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("./dashboard_for_teachers/dashboard.html")
+    # return render_template("./dashboard_for_teachers/dashboard.html")
+    return "<h1>karan</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
