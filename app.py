104678e7edce7ed422fb51ad74ff81127c627330
Karan <karanmandve1234@gmail.com>
2022-10-18 10:27:14 +0530

    adding dashboard endpoint

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def homepage():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("./dashboard_for_teachers/dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
