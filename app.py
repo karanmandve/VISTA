a9605074de8696e26c22f8c1539db017ce4aeff2
Karan <karanmandve1234@gmail.com>
2022-10-21 14:07:18 +0530

    putting all css file to css folder

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
+
 # HOMEPAGE
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("Dashboard_for_teachers/dashboard.html")
-    
+
+
 @app.route("/create_test")
 def create_test():
     return render_template("Dashboard_for_teachers/create_test.html")
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
