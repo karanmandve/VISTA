593f3f40cb959ec67e94caec3b52a35ba1e6af8e
unknown <aa6886461@gmail.com>
2022-10-30 21:01:14 +0530

    added font

--- app.py
+++ app.py
@@ -202,26 +202,7 @@ def students_active():
 
     return jsonify(exam_data[ACTIVE_SUBJECT])
 
-# endpointto store responses
-# @app.route("/response_submit", methods=["POST"])
-# def response_submit():
-#     form_raw_data = request.form.to_dict(flat=False)
-#     form_data = {form_raw_data['test_title'][0] : form_raw_data}
-    
-#     try:
-#         with open("database.txt", "r") as file:
-#             exam_data = file.read()
-#             exam_data = eval(exam_data)
-#         new=form_raw_data['test_title'][0].replace(" ","")
-#         exam_data[f"{new}"] = form_raw_data
 
-#         with open("database.txt", "w") as file:
-#             file.write(f"{exam_data}")
-#     except:
-#         with open("database.txt", "w") as file:
-#             file.write(f"{form_data}")
-
-#     return render_template("Dashboard_for_teachers/dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
