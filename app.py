8c13f77bc5e04fd9a7a129230cf0fbcc583c1001
Krishna Pandey <krishnnapandey@gmail.com>
2022-11-06 12:52:00 +0530

    Added github profile pictures

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, PasswordField, SubmitField, validators
 import random
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "karan"
 
@@ -14,14 +13,12 @@ csrf = CSRFProtect(app)
 cors = CORS(app)
 
 # sql= MySQLPool() #for database
-exam= Exam()
-
+exam = Exam()
 
 DASHBOARD_LOGIN_ID = "root"
 DASHBOARD_LOGIN_PASSWORD = "root"
 ACTIVE_SUBJECT = ""
-SHOW_TEST=""
-
+SHOW_TEST = ""
 
 
 class LoginForm(FlaskForm):
@@ -36,10 +33,11 @@ def homepage():
     global ACTIVE_SUBJECT
     dashboard_login_form = LoginForm()
     with open("database.txt", "r") as file:
-                exam_data = file.read()
-                exam_data = eval(exam_data)
-                ACTIVE_SUBJECT=exam_data["active_subject"]
+        exam_data = file.read()
+        exam_data = eval(exam_data)
+        ACTIVE_SUBJECT = exam_data["active_subject"]
     return render_template("index.html", dashboard_login_form=dashboard_login_form)
+
 
 # about page
 @app.route("/about")
@@ -63,25 +61,25 @@ def dashboard():
             return render_template("Dashboard_for_teachers/dashboard.html")
 
     return redirect(url_for('teacher'))
-    
+
 
 @app.route("/create_test")
 def create_test():
     csrf = LoginForm()
-   
+
     return render_template("Dashboard_for_teachers/create_test.html", csrf=csrf)
 
 
 @app.route("/form-submit", methods=["POST"])
 def form_submit():
     form_raw_data = request.form.to_dict(flat=False)
-    form_data = {form_raw_data['test_title'][0] : form_raw_data}
-    
+    form_data = {form_raw_data['test_title'][0]: form_raw_data}
+
     try:
         with open("database.txt", "r") as file:
             exam_data = file.read()
             exam_data = eval(exam_data)
-            
+
         exam_data[f"{form_raw_data['test_title'][0]}"] = form_raw_data
 
         with open("database.txt", "w") as file:
@@ -100,7 +98,7 @@ def all_exam():
         exam_data = eval(exam_data)
 
     all_exams = {}
-    all_exams = {key:0 for key in exam_data.keys()}
+    all_exams = {key: 0 for key in exam_data.keys()}
     all_exams["active_subject"] = exam_data["active_subject"]
 
     return jsonify(all_exams)
@@ -108,14 +106,14 @@ def all_exam():
 
 # ENDPOINTS FOR DROP DOWN MENU OPTIONS
 
-@app.route("/active-exam/<exam_title>") #first make a dictionary then make it string then pass to url
+@app.route("/active-exam/<exam_title>")  # first make a dictionary then make it string then pass to url
 def active_exam(exam_title):
     global ACTIVE_SUBJECT
-    
-    exam_title = eval(exam_title)# to convert string to dictionary
+
+    exam_title = eval(exam_title)  # to convert string to dictionary
 
     if ACTIVE_SUBJECT == exam_title["subject_name"]:
-        
+
         if not exam_title["is_active"]:
             ACTIVE_SUBJECT = ""
 
@@ -150,7 +148,7 @@ def delete_subject(subject_name):
         exam_data = file.read()
         exam_data = eval(exam_data)
 
-    del exam_data[subject_name.replace("%20"," ")]
+    del exam_data[subject_name.replace("%20", " ")]
 
     with open("database.txt", "w") as file:
         file.write(f"{exam_data}")
@@ -170,21 +168,21 @@ def subject_questions():
 @app.route("/show_test/<subject_name>")
 def show_test(subject_name):
     global SHOW_TEST
-    SHOW_TEST=subject_name
+    SHOW_TEST = subject_name
     if subject_name != ACTIVE_SUBJECT:
         csrf = LoginForm()
         print(ACTIVE_SUBJECT)
-        return render_template("Dashboard_for_teachers/show-test.html",csrf=csrf)
-    else :
+        return render_template("Dashboard_for_teachers/show-test.html", csrf=csrf)
+    else:
         csrf = LoginForm()
         print(ACTIVE_SUBJECT)
-        return render_template("Dashboard_for_teachers/show-active-test.html",csrf=csrf)
+        return render_template("Dashboard_for_teachers/show-active-test.html", csrf=csrf)
 
 
 @app.route("/update_add_subject_questions", methods=["POST"])
 def update_add_subject_questions():
     form_raw_data = request.form.to_dict(flat=False)
-    
+
     with open("database.txt", "r") as file:
         exam_data = file.read()
         exam_data = eval(exam_data)
@@ -195,7 +193,7 @@ def update_add_subject_questions():
         file.write(f"{exam_data}")
 
     return render_template("Dashboard_for_teachers/dashboard.html")
-    
+
 
 # STUDENT SECTION STARTS HERE
 
@@ -215,7 +213,7 @@ def students():
 @app.route("/students_test_page")
 def students_test_page():
     csrf = LoginForm()
-    return render_template("Students/student_test.html",csrf=csrf)
+    return render_template("Students/student_test.html", csrf=csrf)
 
 
 @app.route("/students_active")
@@ -237,12 +235,11 @@ def student_reponse():
             student_data = file.read()
             student_data = eval(student_data)
 
-
         with open("student_response.txt", "w") as file:
             student_data["student"] = response_data
     except:
         with open("student_response.txt", "w") as file:
-            response_data = {"student":response_data}
+            response_data = {"student": response_data}
             file.write(f"{response_data}")
 
     return "more work needed"
@@ -252,10 +249,9 @@ def student_reponse():
 
 @app.route("/generate-passwords/<count>")
 def generate_passwords(count):
-    count=int(count)
-    global STUDENT_PASSWORDS
+    count = int(count)
     passwords = []
-    for i in range(1, count+1):
+    for i in range(1, count + 1):
         password = ''.join(
             random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(6))
         dict1 = {"number": i, "password": password}
@@ -271,8 +267,5 @@ def generate_passwords(count):
 #     return jsonify(STUDENT_PASSWORDS)
 
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
-
