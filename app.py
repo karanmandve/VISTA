9b19b9b59ebf35eb48a47f082a10be77faa9407e
unknown <aa6886461@gmail.com>
2022-11-12 12:58:37 +0530

    testing 2

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from functools import wraps
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "karan"
 app.config["SESSION_COOKIE_SECURE"]=False
+app.config["SERVER_NAME"]="vista.azurewebsites.net"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///exam.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
